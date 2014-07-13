e3bdb16924a9f198506c2968bb799e97d24b857a
Michael Pell <michaelcpell@gmail.com>
2014-07-13 13:44:22 -0600

    Adds plots 3 and 4 plus R files

--- plot1_steps.md
+++ plot1_steps.md
@@ -32,6 +32,25 @@
 #2 end
 
 #3
+  plot(
+    strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
+    data$Sub_metering_1, 
+    type="l",
+    ylab="Global Active Power (kilowatts)",
+    xlab= ""
+  )
+
+  lines(
+    strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
+    data$Sub_metering_2,
+    col="Red"
+  )
+
+  legend(2000,9.5, # places a legend at the appropriate place c("Health","Defense"), # puts text in the legend 
+
+  lty=c(1,1), # gives the legend appropriate symbols (lines)
+
+  lwd=c(2.5,2.5),col=c("blue","red")) # gives the legend lines the correct color and width
 
 #3 end
 
--- plot4.R
+++ plot4.R
@@ -6,13 +6,39 @@ setwd("~/data-workspace/exploring_data")
 
 data <- read.table("trunc_hpc.txt", header=TRUE, sep=";")
 
+par(mfrow = c(2,2))
+
+
+
+
+par(mar= c(5,5,2,.2))
+plot(
+  strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
+  data$Global_active_power, 
+  type="l",
+  ylab="Global Active Power",
+  xlab= "",
+  cex.lab = .8
+)
+
+plot(
+  strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
+  data$Voltage, 
+  type="l",
+  ylab="Voltage",
+  xlab= "datetime",
+  col = "black",
+  cex.lab = .8
+)
+#Plot 3
 plot(
   strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
   data$Sub_metering_1, 
   type="l",
   ylab="Energy sub metering",
   xlab= "",
-  col = "black"
+  col = "black",
+  cex.lab = .8
 )
 
 lines(
@@ -29,8 +55,21 @@ lines(
 
 legend("topright",
        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
-       lwd=c(2.5,2.5),
+       lwd=2.5,
        col=c("black","red","blue"),
        cex=.5,
-       text.width = 30000
-       )
+       text.width = 60000,
+       bty="n"
+)
+
+#Plot 3 End
+
+plot(
+  strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"),
+  data$Global_reactive_power, 
+  type="l",
+  ylab="Global_reactive_power",
+  xlab= "datetime",
+  col = "black",
+  cex.lab = .8
+)
